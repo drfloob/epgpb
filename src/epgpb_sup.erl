29a45caaf6a5fb9c4717ba49286e56425d5a30b6
aj heller <aj@drfloob.com>
2013-01-03 10:52:10 -0800

    allows passing pool specs to epgpb_sup directly

--- src/epgpb_sup.erl
+++ src/epgpb_sup.erl
@@ -3,7 +3,7 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0, start_link/1]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -15,16 +15,23 @@
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+start_link(Pools) ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, Pools).
+
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 init([]) ->
     {ok, Pools} = application:get_env(epgpb, pools),
+    init(Pools);
+init(Pools) ->
     PoolSpecs = lists:map(fun({Name, SizeArgs, WorkerArgs}) ->
 				  PoolArgs = [{name, {local, Name}},
 					      {worker_module, epgpb_worker}] ++ SizeArgs,
 				  poolboy:child_spec(Name, PoolArgs, WorkerArgs)
 			  end, Pools),
     {ok, {{one_for_one, 10, 10}, PoolSpecs}}.
+    
+
 
